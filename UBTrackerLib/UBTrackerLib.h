1e02758b6037690bd64ca2e32eb6af0fc96c0b97
秦志伟 <win_cooperation@163.com>
2017-11-24 10:32:42 +0800

    use GCD_wechat as a demo
    file struct

---
+++ UBTrackerLib/UBTrackerLib.h
@@ -0,0 +1,13 @@
+//
+//  UBTrackerLib.h
+//  UBTrackerLib
+//
+//  Created by qinzhiwei on 17/11/24.
+//  Copyright © 2017年 lobster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UBTrackerLib : NSObject
+
+@end
